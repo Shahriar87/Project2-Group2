0bfc678eb8f5213888e6b3bb47b9357ab558a728
Innovator <shahriar.firoz@gmail.com>
2018-12-12 00:23:47 -0600

    Order table created

--- models/order.js
+++ models/order.js
@@ -1,35 +1,34 @@
 module.exports = function (sequelize, DataTypes) {
     var Order = sequelize.define("Order", {
-        customerName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
+        orderQuantity: {
+            type: DataTypes.INTEGER
         },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            isEmail: true
-        },
-        phone: {
-            type: DataTypes.INTEGER,
-        },
-        quantity: DataTypes.INTEGER
+        totalCost: {
+            type: DataTypes.DECIMAL(13, 2)
+        }
     }, {
             freezeTableName: true,          // Model tableName will be the same as the model name
-            timestamps: false,
-            underscored: true
+            timestamps: true,
+            underscored: false
         });
 
     Order.associate = function (models) {
-        // Toy should belong to an Manufacturer 
+        // Order should belong to an Toy
         Order.belongsTo(models.Toy, {
+            foreignKey: {
+                allowNull: true
+            }
+        });
+    };
+
+    Order.associate = function (models) {
+        // Order should belong to an User
+        Order.belongsTo(models.user, {
             foreignKey: {
                 allowNull: false
             }
         });
     };
-    
+
     return Order;
 };
